c2cb93ccc0ab04e5b1f1a258cd497f30e5068085
willy.ovalle@klarna.com <willy.ovalle@klarna.com>
2019-02-06 16:53:08 +0100

    feat: updating fixture and implementing subcollection tests

--- src/BaseFirestoreRepository.spec.ts
+++ src/BaseFirestoreRepository.spec.ts
@@ -1,13 +1,24 @@
 import MockFirebase from 'mock-cloud-firestore';
 import BaseFirestoreRepository from './BaseFirestoreRepository';
-import { getFixture, User } from '../test/fixture';
+import { getFixture, Album } from '../test/fixture';
 import { expect } from 'chai';
+import { Collection, SubCollection, ISubCollection } from '.';
+
+@Collection('bands')
+export class Band {
+  id: string;
+  name: string;
+  formationYear: number;
+  genres: Array<string>;
+  @SubCollection(Album)
+  albums?: ISubCollection<Album>;
+}
 
 // TODO: explicitely enforce entities to extend {id: string} (IEntity)
-class UserRepository extends BaseFirestoreRepository<User> {}
+class BandRepository extends BaseFirestoreRepository<Band> {}
 
 describe('BaseRepository', () => {
-  let userRep: BaseFirestoreRepository<User> | null = null;
+  let bandRepository: BaseFirestoreRepository<Band> | null = null;
 
   beforeEach(() => {
     const fixture = Object.assign({}, getFixture());
@@ -16,44 +27,45 @@ describe('BaseRepository', () => {
     });
 
     const firestore = firebase.firestore();
-    userRep = new UserRepository(firestore, 'users');
+    bandRepository = new BandRepository(firestore, 'bands');
   });
 
   describe('findById', () => {
     it('must find by id', async () => {
-      const roy = await userRep.findById('roy');
-      expect(roy.id).to.equal('roy');
-      expect(roy.firstName).to.equal('Roy');
+      const pt = await bandRepository.findById('porcupine-tree');
+      expect(pt.id).to.equal('porcupine-tree');
+      expect(pt.name).to.equal('Porcupine Tree');
     });
     it('must return T'); // TODO: Right now roy instanceof User === false, investigate
     it('return null if not found', async () => {
-      const douglas = await userRep.findById('douglas');
-      expect(douglas).to.be.null;
+      const sw = await bandRepository.findById('steven-wilson');
+      expect(sw).to.be.null;
     });
   });
 
   describe('create', () => {
     it('must create item and return T', async () => {
-      const entity = new User();
-      entity.id = 'douglas';
-      entity.firstName = 'Douglas';
-      entity.lastName = 'Reynholm';
-      entity.birthDate = new Date();
+      const entity = new Band();
+      entity.id = 'perfect-circle';
+      entity.name = 'A Perfect Circle';
+      entity.formationYear = 1999;
+      entity.genres = ['alternative-rock', 'alternative-metal', 'hard-rock']
 
-      const douglas = await userRep.create(entity);
-      expect(douglas.id).to.equal(entity.id);
-      expect(douglas.firstName).to.equal(entity.firstName);
-      expect(douglas.lastName).to.equal(entity.lastName);
-      expect(douglas.birthDate).to.equal(entity.birthDate);
+
+      const band = await bandRepository.create(entity);
+      expect(band.id).to.equal(entity.id);
+      expect(band.name).to.equal(entity.name);
+      expect(band.formationYear).to.equal(entity.formationYear);
+      expect(band.genres).to.equal(entity.genres);
     });
   });
 
   describe('update', () => {
     it('must update and return updated item', async () => {
-      const roy = await userRep.findById('roy');
-      roy.lastName = 'Crowd';
-      const updatedRoy = await userRep.update(roy);
-      expect(roy.lastName).to.equal(updatedRoy.lastName);
+      const band = await bandRepository.findById('porcupine-tree');
+      band.name = 'Steven Wilson';
+      const updatedBand = await bandRepository.update(band);
+      expect(band.name).to.equal(updatedBand.name);
     });
     it('must only update changed fields'); // TODO: Discuss
     it('must throw if item is not found');
@@ -61,13 +73,13 @@ describe('BaseRepository', () => {
 
   describe('delete', () => {
     it('must delete item', async () => {
-      await userRep.delete('roy');
-      const roy = await userRep.findById('roy');
+      await bandRepository.delete('porcupine-tree');
+      const roy = await bandRepository.findById('porcupine-tree');
       expect(roy).to.be.null;
     });
+
     it('must throw if item is not found', async () => {
-      // tslint:disable-next-line:no-void-expression
-      expect(await userRep.delete('lol')).to.throw;
+      expect(await bandRepository.delete('lol')).to.throw;
     });
   });
 
@@ -75,63 +87,73 @@ describe('BaseRepository', () => {
     it('must return T[]');
 
     it("must return same list if where filter doesn't apply", async () => {
-      const list = await userRep
-        .whereGreaterOrEqualThan('birthDate', new Date(1950, 12, 12))
+      const list = await bandRepository
+        .whereGreaterOrEqualThan('formationYear', 1983)
         .find();
-      expect(list.length).to.equal(3);
+      expect(list.length).to.equal(2);
     });
 
     it('must filter with whereEqualTo', async () => {
-      const list = await userRep.whereEqualTo('firstName', 'Roy').find();
+      const list = await bandRepository.whereEqualTo('name', 'Porcupine Tree').find();
       expect(list.length).to.equal(1);
-      expect(list[0].firstName).to.equal('Roy');
+      expect(list[0].name).to.equal('Porcupine Tree');
     });
 
     it('must filter with whereGreaterThan', async () => {
-      const list = await userRep
-        .whereGreaterThan('birthDate', new Date('1978-03-09'))
+      const list = await bandRepository
+        .whereGreaterThan('formationYear', 1983)
         .find();
       expect(list.length).to.equal(1);
     });
 
     it('must filter with whereGreaterOrEqualThan', async () => {
-      const list = await userRep
-        .whereGreaterOrEqualThan('birthDate', new Date('1978-03-09'))
+      const list = await bandRepository
+        .whereGreaterOrEqualThan('formationYear', 1983)
         .find();
       expect(list.length).to.equal(2);
     });
 
     it('must filter with whereLessThan', async () => {
-      const list = await userRep
-        .whereLessThan('birthDate', new Date('1978-03-09'))
+      const list = await bandRepository
+        .whereLessThan('formationYear', 1983)
         .find();
       expect(list.length).to.equal(1);
     });
 
     it('must filter with whereLessOrEqualThan', async () => {
-      const list = await userRep
-        .whereLessOrEqualThan('birthDate', new Date('1978-03-09'))
+      const list = await bandRepository
+        .whereLessOrEqualThan('formationYear', 1983)
         .find();
       expect(list.length).to.equal(2);
     });
 
     it('must filter with whereArrayCointain', async () => {
-      const list = await userRep
-        .whereArrayCointain('tags', 'geek')
+      const list = await bandRepository
+        .whereArrayCointain('genres', 'progressive-rock')
         .find();
-      expect(list.length).to.equal(1);
-      expect(list[0].id).to.equal('roy');
+      expect(list.length).to.equal(2);
     });
     it('must filter with two or more operators', async () => {
-      const list = await userRep
-        .whereLessOrEqualThan('birthDate', new Date('1978-03-09'))
-        .whereArrayCointain('tags', 'nerd')
+      const list = await bandRepository
+        .whereLessOrEqualThan('formationYear', 1983)
+        .whereArrayCointain('genres', 'funk-rock')
         .find();
       expect(list.length).to.equal(1);
-      expect(list[0].id).to.equal('moss');
+      expect(list[0].id).to.equal('red-hot-chili-peppers');
     });
   });
 
-  // describe('must handle subcollections');
-  // describe('getRepository');
+  describe('must handle subcollections', () => {
+    it('should initialize subcollections', async () => {
+      const pt = await bandRepository.findById('porcupine-tree');
+      expect(pt.name).to.equal('Porcupine Tree');
+      expect(pt.albums).to.be.instanceOf(BaseFirestoreRepository);
+    });
+
+    it('should be able to execute operations in the subcollection', async () => {
+      const band = await bandRepository.findById('red-hot-chili-peppers');
+      const bestAlbum = await band.albums.findById('stadium-arcadium')
+      expect(bestAlbum.id).to.equal('stadium-arcadium');
+    });
+  });
 });
