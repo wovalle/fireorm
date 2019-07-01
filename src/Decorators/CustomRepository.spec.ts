fc0d9a894f4f75390ad2698144b6e5412eb42425
willy.ovalle@klarna.com <willy.ovalle@klarna.com>
2019-07-01 23:16:42 +0200

    fix: throw if CustomRepository does not inherit from Base
    Co-authored-by: Maximo Dominguez <mamodom@gmail.com>

--- src/Decorators/CustomRepository.spec.ts
+++ src/Decorators/CustomRepository.spec.ts
@@ -1,6 +1,7 @@
 import CustomRepository from './CustomRepository';
 import { expect } from 'chai';
 import { MetadataStorage, Initialize } from '../MetadataStorage';
+import { BaseFirestoreRepository } from '..';
 
 describe('CustomRepositoryDecorator', () => {
   const store = { metadataStorage: new MetadataStorage() };
@@ -16,7 +17,7 @@ describe('CustomRepositoryDecorator', () => {
     }
 
     @CustomRepository(Entity)
-    class EntityRepo {}
+    class EntityRepo extends BaseFirestoreRepository<Entity> {}
 
     const repository = store.metadataStorage.repositories.get(Entity);
     expect(store.metadataStorage.repositories.size).to.eql(1);
@@ -24,6 +25,43 @@ describe('CustomRepositoryDecorator', () => {
     expect(repository.target).to.eql(EntityRepo);
   });
 
-  it('should enforce that custom repository inherits from BaseRepository');
-  it('should only register a repository once');
+  it('should only register a repository once', () => {
+    class Entity {
+      id: string;
+    }
+
+    expect(() => {
+      @CustomRepository(Entity)
+      class EntityRepo extends BaseFirestoreRepository<Entity> {}
+
+      @CustomRepository(Entity)
+      class EntityRepo2 extends BaseFirestoreRepository<Entity> {}
+    }).to.throw;
+  });
+
+  it('should only register a repository once', () => {
+    class Entity {
+      id: string;
+    }
+
+    @CustomRepository(Entity)
+    @CustomRepository(Entity)
+    class EntityRepo extends BaseFirestoreRepository<Entity> {}
+
+    const repository = store.metadataStorage.repositories.get(Entity);
+    expect(store.metadataStorage.repositories.size).to.eql(1);
+    expect(repository.entity).to.eql(Entity);
+    expect(repository.target).to.eql(EntityRepo);
+  });
+
+  it('should enforce that custom repository inherits from BaseRepository', () => {
+    class Entity {
+      id: string;
+    }
+
+    expect(() => {
+      @CustomRepository(Entity)
+      class EntityRepo {}
+    }).to.throw;
+  });
 });
