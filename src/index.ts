fa192273bd1ac8bc42e252e773030a419a738c48
Joe McKie <joe@joemck.ie>
2019-10-30 21:35:15 +0000

    chore: export Initialize from package

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from './Decorators';
 export * from './BaseFirestoreRepository';
 export * from './types';
 export * from './helpers';
-export { initialize } from './MetadataStorage';
+export { initialize, Initialize } from './MetadataStorage';
 
 // Temporary while https://github.com/wovalle/fireorm/issues/58 is being fixed
 export { Type } from 'class-transformer';
