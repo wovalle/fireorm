f626992df9491cc3c852a59d098fc4b53e1b9a06
Willy Ovalle <willyovalle16@gmail.com>
2020-04-26 00:37:18 +0200

    fix: bye tslint, hello eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  ignorePatterns: ['lib/'],
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+      parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+      },
+      extends: [
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin,
+        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+      ],
+      rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+    {
+      files: ['**/*.spec.ts', '**/*.int-spec.ts', 'test/setup.ts'],
+      env: {
+        jest: true,
+        node: true,
+      },
+    },
+  ],
+};
