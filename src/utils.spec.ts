6ce2b6142b8b96a02948f16354caa1fbca9974fa
stefdelec <stephane@arianee.org>
2020-06-22 16:31:17 +0200

    feat: getter properties are extracted from entity

---
+++ src/utils.spec.ts
@@ -0,0 +1,68 @@
+import { Collection, CustomRepository } from './Decorators';
+import { BaseFirestoreRepository } from './BaseFirestoreRepository';
+import { getRepository, getBaseRepository, runTransaction, createBatch } from './helpers';
+import { initialize } from './MetadataStorage';
+import { FirestoreTransaction } from './Transaction/FirestoreTransaction';
+import { FirestoreBatch } from './Batch/FirestoreBatch';
+import { extractAllGetter } from './utils';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const MockFirebase = require('mock-cloud-firestore');
+
+describe('Utils', () => {
+  describe('extractAllGetter', () => {
+    describe('Class', () => {
+      it('should return only getters not data property', () => {
+        class ClassTest {
+          public a = 'a';
+          private _c;
+
+          public get b() {
+            return 'b';
+          }
+        }
+
+        const b = new ClassTest();
+
+        const extracted = extractAllGetter(b);
+        expect(extracted).toEqual({ b: 'b' });
+      });
+
+      it('should return only getters not method', () => {
+        class ClassTest {
+          public get b() {
+            return 'b';
+          }
+
+          public a() {
+            return 'a method';
+          }
+        }
+
+        const b = new ClassTest();
+
+        const extracted = extractAllGetter(b);
+        expect(extracted).toEqual({ b: 'b' });
+      });
+    });
+
+    it('should return only getters which return undefined', () => {
+      class ClassTest {
+        public a = 'a';
+
+        public get b() {
+          return 'b';
+        }
+
+        public get c() {
+          return undefined;
+        }
+      }
+
+      const b = new ClassTest();
+
+      const extracted = extractAllGetter(b);
+      expect(extracted).toEqual({ b: 'b' });
+    });
+  });
+});
