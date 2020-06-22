3e3731362d20186f850fec95f11ec37dfe9132c2
stefdelec <stephane@arianee.org>
2020-06-22 16:31:17 +0200

    feat(serializeEntity): class getter properties are extracted and send to firebase database

--- src/utils.ts
+++ src/utils.ts
@@ -14,9 +14,42 @@ export function serializeEntity<T extends IEntity>(
   obj: T,
   subColMetadata: CollectionMetadata[]
 ): Record<string, any> {
-  const serializableObj = { ...obj };
+
+  const objectGetters = extractAllGetter(obj);
+
+  const serializableObj = { ...obj, ...objectGetters };
+
+
   subColMetadata.forEach(scm => {
     delete serializableObj[scm.propertyKey];
   });
   return serializableObj;
+}
+
+
+export function extractAllGetter(obj): any {
+
+  const prototype = Object.getPrototypeOf(obj);
+  const fromInstanceObj = Object.keys(obj);
+  const fromInstance = Object.getOwnPropertyNames(obj);
+  const fromPrototype = Object.getOwnPropertyNames(Object.getPrototypeOf(obj));
+
+  const keys = [...fromInstanceObj, ...fromInstance, ...fromPrototype];
+
+  const getters = keys
+    .map(key => Object
+      .getOwnPropertyDescriptor(prototype, key))
+    .map((descriptor, index) => {
+      if (descriptor && typeof descriptor.get === 'function') {
+        return keys[index];
+      } else {
+        return undefined;
+      }
+    }).filter(d => d !== undefined);
+
+  return getters.reduce((accumulator, currentValue) => {
+    accumulator[currentValue] = obj[currentValue];
+    return accumulator;
+  }, {});
+
 }
