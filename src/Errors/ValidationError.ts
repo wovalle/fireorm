55092cec2adff58ed21a1b0d87a3ae1cfa3870f1
Willy Ovalle <willyovalle16@gmail.com>
2020-07-07 16:22:11 +0200

    chore: updating ValidationError type

--- src/Errors/ValidationError.ts
+++ src/Errors/ValidationError.ts
@@ -3,34 +3,44 @@
  * Copied from class-validator/validation/ValidationError.d.ts
  */
 export declare class ValidationError {
+
   /**
    * Object that was validated.
    *
    * OPTIONAL - configurable via the ValidatorOptions.validationError.target option
    */
-  target?: Record<string, any>;
+  target?: object;
+
   /**
    * Object's property that haven't pass validation.
    */
   property: string;
+
   /**
    * Value that haven't pass a validation.
    *
    * OPTIONAL - configurable via the ValidatorOptions.validationError.value option
    */
   value?: any;
+
   /**
    * Constraints that failed validation with error messages.
    */
-  constraints: {
-    [type: string]: string;
+  constraints?: {
+      [type: string]: string;
   };
+
   /**
    * Contains all nested validation errors of the property.
    */
   children: ValidationError[];
+
+
+  /*
+   * A transient set of data passed through to the validation result for response mapping
+   */
   contexts?: {
-    [type: string]: any;
+      [type: string]: any;
   };
   /**
    *
