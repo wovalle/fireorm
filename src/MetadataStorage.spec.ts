36659075f9e8ead10ee2266b063390d5402df88b
Willy Ovalle <willyovalle16@gmail.com>
2020-07-24 18:18:31 +0200

    test: adding metadata storage tests

--- src/MetadataStorage.spec.ts
+++ src/MetadataStorage.spec.ts
@@ -1,10 +1,81 @@
-import { MetadataStorage } from './MetadataStorage';
+import { MetadataStorage, CollectionMetadata, RepositoryMetadata } from './MetadataStorage';
+import { BaseFirestoreRepository } from './BaseFirestoreRepository';
+import { IRepository, Constructor } from './types';
 
 describe('MetadataStorage', () => {
-  it.todo('must getCollection by string');
-  it.todo('must getCollection by constructor');
-  it.todo('must setCollections and initialize subcollection metadata');
-  it.todo('must not store repeated collections');
-  it.todo('must getRepositories');
-  it.todo('must setRepositories');
+  let metadataStorage: MetadataStorage = undefined;
+  class Entity {
+    id: string;
+  }
+
+  class SubEntity {
+    id: string;
+  }
+
+  class EntityRepository extends BaseFirestoreRepository<Entity> {}
+
+  const col: CollectionMetadata = {
+    entityConstructor: Entity,
+    name: 'entity',
+    path: 'entity',
+  };
+
+  const subCol: CollectionMetadata = {
+    entityConstructor: SubEntity,
+    name: 'subEntity',
+    path: "['entity', 'subentity']",
+    parentEntityConstructor: Entity,
+    propertyKey: 'subEntities',
+  };
+
+  const entityRepository: RepositoryMetadata = {
+    entity: Entity,
+    target: (EntityRepository as unknown) as Constructor<IRepository<Ien>>,
+  };
+
+  beforeEach(() => {
+    metadataStorage = new MetadataStorage();
+
+    metadataStorage.setCollection(subCol);
+    metadataStorage.setCollection(col);
+    metadataStorage.setRepository(entityRepository);
+  });
+
+  it('must getCollection by string', () => {
+    const entityMetadata = metadataStorage.getCollection('entity');
+
+    expect(entityMetadata.entityConstructor).toEqual(col.entityConstructor);
+    expect(entityMetadata.name).toEqual(col.name);
+    expect(entityMetadata.path).toEqual(col.path);
+    expect(entityMetadata.subCollections.length).toEqual(1);
+  });
+
+  it('must getCollection by constructor', () => {
+    const entityMetadata = metadataStorage.getCollection(Entity);
+
+    expect(entityMetadata.entityConstructor).toEqual(col.entityConstructor);
+    expect(entityMetadata.name).toEqual(col.name);
+    expect(entityMetadata.path).toEqual(col.path);
+    expect(entityMetadata.subCollections.length).toEqual(1);
+  });
+
+  it('must initialize subcollection metadata', () => {
+    const entityMetadata = metadataStorage.getCollection('entity');
+
+    expect(entityMetadata.subCollections.length).toEqual(1);
+    expect(entityMetadata.subCollections[0]).toEqual(subCol);
+  });
+
+  it('must not store repeated collections', () => {
+    metadataStorage.setCollection(col);
+    expect(metadataStorage.collections.filter(c => c.entityConstructor === Entity).length).toEqual(
+      1
+    );
+  });
+  it('must getRepositories', () => {
+    const repo = metadataStorage.getRepository(Entity);
+
+    expect(repo.entity).toEqual(entityRepository.entity);
+    expect(repo.target).toEqual(entityRepository.target);
+  });
 });
