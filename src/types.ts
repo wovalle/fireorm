b10c7049dece67256d1b12a72bf48a7f1a6c30e8
Andy Garvin <garvin.andy@gmail.com>
2020-12-15 07:25:17 -0600

    feat(query): remove another unused import

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { OrderByDirection, DocumentReference, QuerySnapshot } from '@google-cloud/firestore';
+import { OrderByDirection, DocumentReference } from '@google-cloud/firestore';
 
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 export type PartialWithRequiredBy<T, K extends keyof T> = Pick<T, K> & Partial<Omit<T, K>>;
